126b40e9b7236324bf8f53da8e49a0d6b17f8c70
lamelemon <s4laee03@students.osao.fi>
2025-12-04 12:06:56 +0200

    base project

---
+++ src/main/kotlin/io/github/lamelemon/blockprot/Blockprot.kt
@@ -0,0 +1,14 @@
+package io.github.lamelemon.blockprot
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Blockprot : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
