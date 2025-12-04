9d0ce8e51d4bda0f4c370bcc24b14bcd216e4a53
lamelemon <s4laee03@students.osao.fi>
2025-12-04 14:03:29 +0200

    add helper functions for block friends and owners

---
+++ src/main/kotlin/io/github/lamelemon/blockprot/utils/UUIDDataType.kt
@@ -0,0 +1,33 @@
+package io.github.lamelemon.blockprot.utils
+
+import org.bukkit.persistence.PersistentDataAdapterContext
+import org.bukkit.persistence.PersistentDataType
+import java.nio.ByteBuffer
+import java.util.*
+
+object UUIDDataType: PersistentDataType<ByteArray, UUID> {
+
+    override fun getPrimitiveType(): Class<ByteArray> = ByteArray::class.java
+
+    override fun getComplexType(): Class<UUID> = UUID::class.java
+
+    override fun toPrimitive(
+        complex: UUID,
+        context: PersistentDataAdapterContext
+    ): ByteArray {
+        val bb = ByteBuffer.allocate(Long.SIZE_BYTES * 2)
+        bb.putLong(complex.mostSignificantBits)
+        bb.putLong(complex.leastSignificantBits)
+        return bb.array()
+    }
+
+    override fun fromPrimitive(
+        primitive: ByteArray,
+        context: PersistentDataAdapterContext
+    ): UUID {
+        val bb = ByteBuffer.wrap(primitive)
+        val firstLong = bb.getLong()
+        val secondLong = bb.getLong()
+        return UUID(firstLong, secondLong)
+    }
+}
--- src/main/kotlin/io/github/lamelemon/blockprot/utils/utils.kt
+++ src/main/kotlin/io/github/lamelemon/blockprot/utils/utils.kt
@@ -1,17 +1,65 @@
 package io.github.lamelemon.blockprot.utils
 
 import io.github.lamelemon.blockprot.Blockprot.Companion.instance
+import io.github.lamelemon.blockprot.utils.UUIDListDataType.addUuid
+import io.github.lamelemon.blockprot.utils.UUIDListDataType.removeUuid
+import org.bukkit.Bukkit
+import org.bukkit.Material
 import org.bukkit.NamespacedKey
 import org.bukkit.entity.Player
+import org.bukkit.persistence.PersistentDataContainer
+import java.util.UUID
 
 object utils {
-    val friendKey: NamespacedKey = NamespacedKey(instance, "friends")
+    private val friendKey: NamespacedKey = NamespacedKey(instance, "friends")
+    private val ownerKey: NamespacedKey = NamespacedKey(instance, "owner")
 
     fun messagePlayer(player: Player, message: String) {
         player.sendRichMessage("<gold>[</gold><blue>BlockProt</blue><gold>]</gold> $message")
     }
 
-    fun addPlayerFriend(player: Player, friend: Player) {
-        TODO("not added yet")
+    fun addFriend(player: Player, friend: Player) {
+        player.persistentDataContainer.addUuid(friendKey, friend.uniqueId)
+    }
+
+    fun addFriend(dataContainer: PersistentDataContainer, friend: Player) {
+        dataContainer.addUuid(friendKey, friend.uniqueId)
+    }
+
+    fun removeFriend(player: Player, friend: Player) {
+        player.persistentDataContainer.removeUuid(friendKey, friend.uniqueId)
+    }
+
+    fun removeFriend(dataContainer: PersistentDataContainer, friend: Player) {
+        dataContainer.removeUuid(friendKey, friend.uniqueId)
+    }
+
+    fun isFriend(dataContainer: PersistentDataContainer, friend: Player): Boolean {
+        return dataContainer.get(friendKey, UUIDListDataType)?.contains(friend.uniqueId) ?: false
+    }
+
+    fun setOwner(dataContainer: PersistentDataContainer, owner: Player) {
+        dataContainer.set(ownerKey, UUIDDataType, owner.uniqueId)
+    }
+
+    fun getOwner(dataContainer: PersistentDataContainer): UUID? {
+        return dataContainer.get(ownerKey, UUIDDataType)
+    }
+
+    fun removeOwner(dataContainer: PersistentDataContainer) {
+        if (dataContainer.has(ownerKey)) {
+            dataContainer.remove(ownerKey)
+        }
+    }
+
+    fun isAllowedToInteract(dataContainer: PersistentDataContainer, player: Player): Boolean {
+        if (isFriend(dataContainer, player)) return true
+
+        val owner = getOwner(dataContainer)?.let { Bukkit.getPlayer(it) }
+        return if (owner is Player) {
+            isFriend(owner.persistentDataContainer, player)
+        } else {
+            getOwner(dataContainer)?.let { Bukkit.getOfflinePlayer(it) }?.persistentDataContainer?.get(friendKey, UUIDListDataType)?.contains(player.uniqueId) == true
+        }
     }
 }
