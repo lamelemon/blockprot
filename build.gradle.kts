fba6297eca696d59a189f0f1d6ca7a693c4dd2dc
lamelemon <191560603+lamelemon@users.noreply.github.com>
2026-01-17 10:39:44 +0200

    Fix most bugs involving double chests

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "io.github.lamelemon"
-version = "1.1.2"
+version = "1.1.4"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/io/github/lamelemon/blockprot/events/block/BlockInteract.kt
+++ src/main/kotlin/io/github/lamelemon/blockprot/events/block/BlockInteract.kt
@@ -1,8 +1,11 @@
 package io.github.lamelemon.blockprot.events.block
 
 import com.destroystokyo.paper.event.block.BlockDestroyEvent
+import io.github.lamelemon.blockprot.BlockProt.Companion.instance
 import io.github.lamelemon.blockprot.utils.BlockOwnerDialog
 import io.github.lamelemon.blockprot.utils.Utils
+import jdk.jshell.execution.Util
+import org.bukkit.Bukkit
 import org.bukkit.Sound
 import org.bukkit.block.Block
 import org.bukkit.block.Chest
@@ -36,28 +39,44 @@ class BlockInteract: Listener {
         if (!Utils.isAllowedToInteract(dataContainer, player)) {
             event.setUseInteractedBlock(Event.Result.DENY)
             Utils.messagePlayer(player,"<red>You are not allowed to interact with this block!</red>")
+            return
         }
-        else if (blockState is Chest && (blockState.blockData as ChestData).type != ChestData.Type.SINGLE) {
+
+        if (blockState is Chest && (blockState.blockData as ChestData).type != ChestData.Type.SINGLE) {
             // We can make all of these type casts because we know things beforehand that don't seem to be known
             val doubleChest = blockState.inventory.holder as DoubleChest
-            val otherChest = if ((blockState.blockData as ChestData).type == ChestData.Type.LEFT) doubleChest.leftSide
-            else doubleChest.rightSide
+            val otherChest: Chest = (if ((blockState.blockData as ChestData).type == ChestData.Type.LEFT) doubleChest.leftSide
+            else doubleChest.rightSide) as Chest
 
-            val otherDataContainer = (otherChest as Chest).persistentDataContainer
+            val otherDataContainer = otherChest.persistentDataContainer
+            val otherChestOwner = Utils.getOwner(otherDataContainer)
             if (!Utils.isAllowedToInteract(otherDataContainer, player)) {
-                Utils.setOwner(dataContainer, Utils.getOwner(otherDataContainer) as UUID)
+                Utils.setOwner(dataContainer, otherChestOwner as UUID)
                 event.setUseInteractedBlock(Event.Result.DENY)
                 Utils.messagePlayer(player,"<red>You are not allowed to interact with this block!</red>")
                 return
-            } else if (Utils.isOwner(otherDataContainer, player) && !Utils.hasOwner(dataContainer)) {
-                Utils.setOwner(dataContainer, player)
-                Utils.messagePlayer(player, "<green>Took ownership of ${block.type.name.lowercase().replaceFirstChar { it.titlecase(getDefault()) }}</green>")
+            }
+
+            val interactedChestOwner = Utils.getOwner(dataContainer)
+            val interactedChestUnOwned = interactedChestOwner !is UUID
+            val otherChestUnOwned = otherChestOwner !is UUID
+            val playerID = player.uniqueId
+            when {
+                interactedChestUnOwned && otherChestUnOwned -> {
+                    Utils.messagePlayer(player, "<green>Took ownership of ${block.type.name.lowercase().replaceFirstChar { it.titlecase(getDefault()) }}</green>")
+                    Utils.setOwner(blockState, player)
+                    Utils.setOwner(otherChest, player)
+                }
+                interactedChestUnOwned && playerID == otherChestOwner -> Utils.setOwner(otherChest, player)
+                otherChestUnOwned && playerID == interactedChestOwner -> Utils.setOwner(blockState, player)
             }
         }
-        else if (player.isSneaking && player.inventory.itemInMainHand.isEmpty) {
+
+        if (player.isSneaking && player.inventory.itemInMainHand.isEmpty) {
             if (Utils.isOwner(dataContainer, player) || (Utils.hasOwner(dataContainer) && player.hasPermission("blockprot.permission.admin.unlock"))) {
-                event.setUseInteractedBlock(Event.Result.DENY)
+                player.closeInventory()
                 BlockOwnerDialog(player, block, blockState)
+                event.setUseInteractedBlock(Event.Result.DENY)
             }
             else if (!Utils.hasOwner(dataContainer)) {
                 Utils.setOwner(blockState, player)
@@ -83,7 +102,8 @@ class BlockInteract: Listener {
         val blockState = event.block.state
         if (blockState !is TileState) return
 
-        if (!Utils.isOwner(blockState.persistentDataContainer, event.player)) {
+        val ownerUUID = Utils.getOwner(blockState.persistentDataContainer)
+        if (ownerUUID !is UUID || ownerUUID == event.player.uniqueId) {
             event.isCancelled = true
             Utils.notifyPlayer(event.player, "<red>You are not allowed to break this block!</red>", Sound.BLOCK_NOTE_BLOCK_PLING)
         }
